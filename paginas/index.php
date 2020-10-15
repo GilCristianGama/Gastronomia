8d8e00a73f5c71bbe700edaad359c9d55b1cc208
GilCristianGama <ppgama50@gmail.com>
2020-10-15 09:52:26 -0300

    arquivobase

--- paginas/index.php
+++ paginas/index.php
@@ -1,18 +1,31 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gastronomia</title>
+    <title>Gastronomi</title>
+
 </head>
+
 <body>
-    
-<div class="alert alert-warning alert-dismissible fade show" role="alert">
-  <strong>Oloco, meu!</strong> Olha esse alerta animado, como é chique!
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-    <span aria-hidden="true">&times;</span>
-  </button>
-</div> 
+
+
+
+    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <strong>Oloco, meu!</strong> Olha esse alerta animado, como é chique!
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+
+    </div>
+
+
+
+
+
+
 
 </body>
+
 </html>
